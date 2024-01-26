d8440fb27222021e4c9dd12460a9ace147d1322a
Fairson Soares <Fairson123@hotmail.com>
2024-01-26 20:34:58 +0000

    scrapper

--- scraper.py
+++ scraper.py
@@ -11,5 +11,13 @@ r = requests.get(url)
 
 if r.status_code == 200:
     soup = BeautifulSoup(r.content, 'html.parser')
-    poems = soup.find_all('a')
-    print(poems)
+    poems = soup.find_all("ul" , {"class" : "c-vList c-vList_bordered c-vList_bordered_anomaly"})
+    for index, poem in enumerate(poems):
+        title = poem.find('h2')
+        links = poem.find("a", href=True)["href"]
+        x = requests.get(links)
+        content = BeautifulSoup(x.content, 'html.parser').text
+        content_encode = bytes(content.encode())
+        file = open(f" poem {index}", "wb")
+        file.write(content_encode)
+    
