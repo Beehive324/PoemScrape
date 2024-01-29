85cd6b533c8a6c829ae59d7480620755c4b463ec
Fairson Soares <Fairson123@hotmail.com>
2024-01-29 20:45:10 +0000

    scrapper

--- scraper.py
+++ scraper.py
@@ -2,8 +2,10 @@ import requests
 import json
 import string
 from bs4 import BeautifulSoup
+from datetime import date
+import datetime
 
-
+today = str(date.today())
 
 url = "https://www.poetryfoundation.org/poems/poem-of-the-day"
 
@@ -12,17 +14,19 @@ r = requests.get(url)
 if r.status_code == 200:
     soup = BeautifulSoup(r.content, 'html.parser')
     poems = soup.findAll('article')
+    print(poems)
 
-while True:
-    for index, poem in enumerate(poems, start=0):
+
+    for poem in poems:
         link = poem.find('h2', {'class':'c-hdgSans c-hdgSans_2'})
         link = link.find('a')['href']
         x = requests.get(link)
         soup_2 = BeautifulSoup(x.content, 'html.parser')
-        title = soup_2.find('h1', {'class' : 'c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline'}).text
-        content = soup_2.find('div', {'class': 'c-feature-bd'}).text
+        title = soup_2.find('h1', {'class' : 'c-hdgSans c-hdgSans_2 c-mix-hdgSans_inline'}).string
+        print(title.text)
+        content = soup_2.find('div', {'class': 'c-feature'}).text
         content_encode = bytes(content.encode())
-        file = open(f" poem {index} {title}", "wb")
+        file = open(f"{today}", "wb")
         file.write(content_encode)
         file.close()
         print("Saved")
